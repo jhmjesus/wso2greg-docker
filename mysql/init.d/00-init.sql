eb13972e21f2a08227dcbe7a4b836b15b1620132
José Horácio <31861096+jhmjesus@users.noreply.github.com>
2024-06-14 20:27:29 -0300

    fix crlf x lf .sh scripts and some Dockerfile changes
    fix crlf x lf .sh scripts and some Dockerfile changes

---
+++ mysql/init.d/00-init.sql
@@ -0,0 +1,5 @@
+GRANT ALL PRIVILEGES ON WSO2CARBON_DB.* TO 'wso2carbon'@'localhost' identified by 'wso2carbon';
+GRANT ALL PRIVILEGES ON WSO2CARBON_DB.* TO 'wso2carbon'@'127.0.0.1' identified by 'wso2carbon';
+GRANT ALL PRIVILEGES ON WSO2CARBON_DB.* TO 'wso2carbon'@'greg' identified by 'wso2carbon';
+
+FLUSH PRIVILEGES;
